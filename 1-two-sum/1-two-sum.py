3e76025075bc43d22b361916a9b5304048e3c692
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-04-02 22:01:36 +0530

    Time: 99 ms (56.43%), Space: 15.1 MB (51.58%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        Dict={}
+        
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            
+            if target-nums[i] in Dict:
+                return[Dict[target-nums[i]],i]
                 
-                if(nums[i]+nums[j]==target):
-                    return [i,j]
-                
+            Dict[nums[i]]=i
                 
         
