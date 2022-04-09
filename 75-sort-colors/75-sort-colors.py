418eac1d0ceb1f66c7b26aedf6648fe212579a53
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-04-09 22:28:45 +0530

    Time: 49 ms (47.25%), Space: 14 MB (17.83%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,6 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n=len(nums)
         
-        return nums.sort()
+        for i in range(n):
+            swapped=False
+            for j in range(0,n-i-1):
+                
+                if(nums[j]>nums[j+1]):
+                    
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+                    swapped=True
+            
+            if(swapped==False):
+                break
+        
+        
         
