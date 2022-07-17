d9a9e2da2c299ea1b6465cf8c1ebadf8221ccf9a
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-07-17 20:26:34 +0530

    Time: 50 ms (43.18%), Space: 13.8 MB (95.68%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -9,7 +9,7 @@ class Solution:
         slow=head
         fast=head
         
-        while(fast!=None and fast.next!=None):
+        while(fast and fast.next):
             
             slow=slow.next
             fast=fast.next.next
