ddd244407a7c1de866a91f94717ffe940d2f6cd9
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-08-02 10:37:42 +0530

    Time: 402 ms (73.52%), Space: 16.4 MB (80.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -5,27 +5,23 @@ class Solution:
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                
-                if grid[i][j]=="1":
-                    self.dfs(i,j,grid)
+                if(grid[i][j]=="1"):
                     islands+=1
+                    self.dfs(i,j,grid)
         
         return islands
         
+    
     def dfs(self,r,c,grid):
         
-        
-        
         if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]):
-            return 
+            return
         
-        if grid[r][c]=="1":
+        if(grid[r][c]=="1"):
             grid[r][c]="0"
             self.dfs(r+1,c,grid)
             self.dfs(r-1,c,grid)
             self.dfs(r,c+1,grid)
             self.dfs(r,c-1,grid)
             
-        
-        
-        
+            
