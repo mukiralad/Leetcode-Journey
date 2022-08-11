6ba1103b92dca8fff9ee8f33640950e26c297e3f
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-08-11 12:02:54 +0530

    Time: 82 ms (6.41%), Space: 15.4 MB (94.21%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -9,12 +9,12 @@ class Solution:
         prev=None
         curr=head
         
-        while curr is not None:
+        while(curr is not None):
+            
             temp=curr.next
             curr.next=prev
             prev=curr
             curr=temp
         
         return prev
-            
         
