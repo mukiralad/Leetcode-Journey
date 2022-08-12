442acae0eb39e9835b3e0804a40cb69837705bbd
mukiralad <67241568+mukiralad@users.noreply.github.com>
2022-08-12 20:49:23 +0530

    Time: 43 ms (85.17%), Space: 13.9 MB (79.56%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -13,21 +13,23 @@ class Solution:
             if(list1.val<list2.val):
                 temp.next=list1
                 list1=list1.next
-            
+          
             else:
                 temp.next=list2
                 list2=list2.next
-            
             temp=temp.next
-        
+              
         
         if list1:
             temp.next=list1
-        
-        elif list2:
+        if list2:
             temp.next=list2
         
         return dummy.next
+        
+        
+        
+        
         
         
         
